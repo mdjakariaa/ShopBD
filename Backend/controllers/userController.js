17d4b69fb05d29ff1c3cac015e2d66e3b015e2f1
jakaria <admin@jakaria.com>
2026-08-20 09:28:40 +0600

    add user authentication(not fully, only function tests), routes and integrate with server

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,12 @@
+// Route for user login
+const loginUser = async (req, res) => {};
+
+// Route for user register
+const registerUser = async (req, res) => {
+    res.json({ message: "User registered successfully" });
+};
+
+// Route for admin login
+const adminLogin = async (req, res) => {};
+
+export { loginUser, registerUser, adminLogin };
