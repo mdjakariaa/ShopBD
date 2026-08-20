5ad523b6ebe3d4cfba4da0598788bec85ba28674
jakaria <admin@jakaria.com>
2026-08-20 08:49:44 +0600

    add Cloudinary configuration and update environment variables

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   mongoose.connection.on("connected", () => {
-    console.log("DB Connected");
+    console.log("DB Connected"); // This will log when the database connection is successfully established
   });
 
   await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
