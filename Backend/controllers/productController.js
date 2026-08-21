c0fdd33b7a60468ae5a844bf015d3c13b86c810d
jakaria <admin@jakaria.com>
2026-08-21 11:13:34 +0600

    Implemented addProduct functionality with image upload support

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,7 +1,53 @@
 
 
 // function for add product
-const addProduct = async (req, res) => {};
+const addProduct = async (req, res) => {
+    try {
+      // destructuring the product data from request body
+      const {
+        name,
+        description,
+        price,
+        category,
+        subCategory,
+        sizes,
+        bestseller,
+      } = req.body;
+
+      // destructuring the product images from request files
+      const image1 = req.files.image1 ? req.files.image1[0] : null; // Check if image1 exists in req.files and assign it, otherwise assign null
+      const image2 = req.files.image2 ? req.files.image2[0] : null;
+      const image3 = req.files.image3 ? req.files.image3[0] : null;
+      const image4 = req.files.image4 ? req.files.image4[0] : null;
+
+      // const image1 = req.files.image1 && req.files.image1[0];
+      // const image2 = req.files.image2 && req.files.image2[0];
+      // const image3 = req.files.image3 && req.files.image3[0];
+      // const image4 = req.files.image4 && req.files.image4[0];
+
+      console.log(
+        name,
+        description,
+        price,
+        category,
+        subCategory,
+        sizes,
+        bestseller,
+      );
+
+      console.log(image1, image2, image3, image4);
+
+      res.json({});
+
+    } catch (error) {
+      console.log(error);
+
+      res.json({
+        success: false,
+        message: error.message,
+      });
+    }
+};
 
 
 // function for list product
--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -1,4 +1,5 @@
 import express from "express";
+import upload from "../middleware/multer.js";
 
 import {
   listProducts,
@@ -9,7 +10,16 @@ import {
 
 const productRouter = express.Router();
 
-productRouter.post("/add", addProduct);
+productRouter.post(
+  "/add",
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct,
+);
 productRouter.post("/remove", removeProduct);
 productRouter.post("/single", singleProduct);
 productRouter.get("/list", listProducts);
