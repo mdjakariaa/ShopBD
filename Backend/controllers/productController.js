0442923216ebf8ad11e5c95877f7e2ee0a5b06f5
jakaria <admin@jakaria.com>
2026-08-22 09:54:15 +0600

    get the List of saved Products from the Database

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -72,7 +72,15 @@ const addProduct = async (req, res) => {
 };
 
 // function for list product
-const listProducts = async (req, res) => {};
+const listProducts = async (req, res) => {
+  try {
+    const products = await productModel.find({});
+    res.json({ success: true, products });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // function for removing product
 const removeProduct = async (req, res) => {};
