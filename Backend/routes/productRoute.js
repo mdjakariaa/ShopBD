6c8188dcb69759370b124f3f934654fe1483ba65
jakaria <admin@jakaria.com>
2026-08-23 09:49:22 +0600

    Added adminLogin & adminAuthentication to add and remove the products

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -7,11 +7,12 @@ import {
   removeProduct,
   singleProduct,
 } from "../controllers/productController.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter = express.Router();
 
 productRouter.post(
-  "/add",
+  "/add",adminAuth,
   upload.fields([
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 },
@@ -20,7 +21,7 @@ productRouter.post(
   ]),
   addProduct,
 );
-productRouter.post("/remove", removeProduct);
+productRouter.post("/remove", adminAuth, removeProduct);
 productRouter.post("/single", singleProduct);
 productRouter.get("/list", listProducts);
 
